a48275250b858c42fcce9353e68251cf7c1d189e
Philip Leblanc <pal_101@hotmail.com>
2017-11-26 18:33:23 -0800

    Part 5 - Add circular buffer. Change myIO to use circ buffer. Add files from other parts to have if ever needed

--- Ensc351x-myio-part3/Debug/subdir.mk
+++ Ensc351x-myio-part3/Debug/subdir.mk
@@ -4,29 +4,20 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/Ensc351Part1.cpp \
-../src/PeerX.cpp \
-../src/SenderX.cpp \
-../src/myIO.cpp 
+../myIO.cpp 
 
 OBJS += \
-./src/Ensc351Part1.o \
-./src/PeerX.o \
-./src/SenderX.o \
-./src/myIO.o 
+./myIO.o 
 
 CPP_DEPS += \
-./src/Ensc351Part1.d \
-./src/PeerX.d \
-./src/SenderX.d \
-./src/myIO.d 
+./myIO.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-src/%.o: ../src/%.cpp
+%.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++1y -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -std=c++1y -I"/media/sf_U_ensc251/workspace-cpp-OxygenJune-351/Ensc351x-myio" -I"/media/sf_U_ensc251/git/ensc351lib/Ensc351" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
